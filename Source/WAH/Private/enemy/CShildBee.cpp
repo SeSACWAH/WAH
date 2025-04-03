72d03cb89b8324da88348a77cf4860e38ed7faab
DESKTOP-M750EOB\MSI <kkmo9407@gmail.com>
2025-04-03 20:12:53 +0900

    첫커밋

---
+++ Source/WAH/Private/enemy/CShildBee.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "enemy/CShildBee.h"
+
+// Sets default values
+ACShildBee::ACShildBee()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACShildBee::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACShildBee::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
