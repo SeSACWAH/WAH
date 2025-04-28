8678cb52f0b44c6e9993efc29b46cfd3b11efb9c
D0YUN <191944956+D0YUN@users.noreply.github.com>
2025-04-28 22:04:41 +0900

    [DYL] 방 검색

--- Source/WAH/Private/CWAHGameInstance.cpp
+++ Source/WAH/Private/CWAHGameInstance.cpp
@@ -3,6 +3,7 @@
 
 #include "CWAHGameInstance.h"
 #include "Interfaces/OnlineSessionInterface.h"
+#include "../../../../../../../Plugins/Online/OnlineBase/Source/Public/Online/OnlineSessionNames.h"
 
 
 void UCWAHGameInstance::Init()
@@ -13,6 +14,8 @@ void UCWAHGameInstance::Init()
 		sessionInterface = subsys->GetSessionInterface();
 
 		sessionInterface->OnCreateSessionCompleteDelegates.AddUObject(this, &UCWAHGameInstance::OnCreateSessionComplete);
+		sessionInterface->OnFindSessionsCompleteDelegates.AddUObject(this,
+			&UCWAHGameInstance::OnFindSessionsComplete);
 
 		FTimerHandle handle;
 		GetWorld()->GetTimerManager().SetTimer(handle, FTimerDelegate::CreateLambda([&]
@@ -21,6 +24,13 @@ void UCWAHGameInstance::Init()
 
 			}
 		), 2.0f, false);
+
+		FTimerHandle handle2;
+		GetWorld()->GetTimerManager().SetTimer(handle2, FTimerDelegate::CreateLambda([&]
+			{
+				FindOtherSession();
+			}), 2.0f, false);
+
 	}
 }
 
@@ -53,10 +63,10 @@ void UCWAHGameInstance::CreateMySession(FString roomName, int32 playerCount)
 	sessionSettings.NumPublicConnections = playerCount;
 
 	// 7. 커스텀 룸네임 설정
-	sessionSettings.Set(FName("ROOM_NAME"), roomName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+	sessionSettings.Set(FName("ROOM_NAME"), StringBase64Encode(roomName), EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 
 	// 8. 호스트 네임 설정
-	sessionSettings.Set(FName("HOST_NAME"), mySessionName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
+	sessionSettings.Set(FName("HOST_NAME"), StringBase64Encode(mySessionName), EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 
 	// netID
 	FUniqueNetIdPtr netID = GetWorld()->GetFirstLocalPlayerFromController()->GetUniqueNetIdForPlatformUser().GetUniqueNetId();
@@ -69,4 +79,89 @@ void UCWAHGameInstance::CreateMySession(FString roomName, int32 playerCount)
 void UCWAHGameInstance::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Session Name : %s, bWasSuccessful : %d"), *SessionName.ToString(), bWasSuccessful);
+}
+
+void UCWAHGameInstance::FindOtherSession()
+{
+	sessionSearch = MakeShareable(new FOnlineSessionSearch());
+
+	// 1. 세션 검색 조건 설정
+	sessionSearch->QuerySettings.Set(SEARCH_PRESENCE, true, EOnlineComparisonOp::Equals);
+
+	// 2. Lan 여부
+	sessionSearch->bIsLanQuery = IOnlineSubsystem::Get()->GetSubsystemName() == FName("NULL");
+
+	// 3. 최대 검색 세션 수
+	sessionSearch->MaxSearchResults = 10;
+
+	// 4. 세션검색
+	sessionInterface->FindSessions(0, sessionSearch.ToSharedRef());
+
+}
+
+void UCWAHGameInstance::OnFindSessionsComplete(bool bWasSuccessful)
+{
+	// 찾기 실패시
+	if (!bWasSuccessful)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Session search failed..."));
+		return;
+	}
+
+	// 세션검색결과 배열
+	auto results = sessionSearch->SearchResults;
+	UE_LOG(LogTemp, Warning, TEXT("Search Result Count : %d"), results.Num());
+
+	// 정보를 가져온다
+	for (int i = 0; i < results.Num(); ++i)
+	{
+		auto sr = results[i];
+
+		// 정보가 유효하지 체크
+		if (!sr.IsValid()) continue;
+
+		FString roomName;
+		FString hostName;
+
+		FSessionInfo sessionInfo;
+		sessionInfo.index = i;
+
+		sr.Session.SessionSettings.Get(FName("ROOM_NAME"), roomName);
+		sessionInfo.roomName = StringBase64Decode(roomName);
+
+		sr.Session.SessionSettings.Get(FName("HOST_NAME"), hostName);
+		sessionInfo.hostName = StringBase64Decode(hostName);
+
+		// 입장가능한 플레이어 수
+		int32 maxPlayerCount = sr.Session.SessionSettings.NumPublicConnections;
+		// 현재 입장한 플레이어 수 ( 최대 - 현재 입장 가능한 수 )
+		// NumOpenPublicConnections 스팀에서만 정상적으로 값이 들어온다.
+		int32 currentPlayerCount = maxPlayerCount - sr.Session.NumOpenPublicConnections;
+
+		sessionInfo.playerCount = FString::Printf(TEXT("(%d/%d)"), currentPlayerCount, maxPlayerCount);
+
+		// 핑정보 (스팀에서는 9999로 나온다)
+		int32 pingSpeed = sr.PingInMs;
+
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *sessionInfo.ToString());
+	}
+
+}
+
+FString UCWAHGameInstance::StringBase64Encode(const FString& str)
+{
+	// Set 할 때 : FStirng -> UTF8 (std::string) -> TArray<uint8> -> base64 로 Encode
+	std::string utf8String = TCHAR_TO_UTF8(*str);
+	TArray<uint8> arrayData = TArray<uint8>((uint8*)(utf8String.c_str()), utf8String.length());
+	return FBase64::Encode(arrayData);
+}
+
+FString UCWAHGameInstance::StringBase64Decode(const FString& str)
+{
+	// Get할 때 (서버에서 방 정보들을 받아올 때)
+	// : base64로 Decode -> TArray<uint8> -> TCHAR
+	TArray<uint8> arrayData;
+	FBase64::Decode(str, arrayData);
+	std::string utf8String((char*)(arrayData.GetData()), arrayData.Num());
+	return UTF8_TO_TCHAR(utf8String.c_str());
 }
--- Source/WAH/Public/CWAHGameInstance.h
+++ Source/WAH/Public/CWAHGameInstance.h
@@ -7,9 +7,28 @@
 #include "OnlineSessionSettings.h"
 #include "CWAHGameInstance.generated.h"
 
-/**
- * 
- */
+USTRUCT(BlueprintType)
+struct FSessionInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadOnly)
+	FString roomName;
+	UPROPERTY(BlueprintReadOnly)
+	FString hostName;
+	UPROPERTY(BlueprintReadOnly)
+	FString playerCount;
+	UPROPERTY(BlueprintReadOnly)
+	int32 pingSpeed;
+	UPROPERTY(BlueprintReadOnly)
+	int32 index;
+
+	inline FString ToString()
+	{
+		return FString::Printf(TEXT("[%d] %s : %s - %s, %dms"), index, *roomName, *hostName, *playerCount, pingSpeed);
+	}
+};
+
 UCLASS()
 class WAH_API UCWAHGameInstance : public UGameInstance
 {
@@ -28,4 +47,14 @@ public:
 	UFUNCTION()
 	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
 
+	// 방 검색
+	TSharedPtr<FOnlineSessionSearch> sessionSearch;
+
+	void FindOtherSession();
+
+	void OnFindSessionsComplete(bool bWasSuccessful);
+
+	// 다국어 인코딩
+	FString StringBase64Encode(const FString& str);
+	FString StringBase64Decode(const FString& str);
 };
